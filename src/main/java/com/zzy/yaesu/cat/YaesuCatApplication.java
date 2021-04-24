7785c2b5b3a277528926d3a38dde196d38ac0429
BH9EQH <504325666@qq.com>
2021-04-25 00:10:19 +0800

    首次导入沙雕项目

---
+++ src/main/java/com/zzy/yaesu/cat/YaesuCatApplication.java
@@ -0,0 +1,13 @@
+package com.zzy.yaesu.cat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaesuCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaesuCatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zzy/yaesu/cat/YaesuCatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzy.yaesu.cat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaesuCatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
